8e5f4981f18c02be6358702ec9c28d8e552e8c39
murali-a <69081940+murali-a@users.noreply.github.com>
2020-09-19 22:16:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var Stand
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  slingshot.fly();
+}
+function keyPressed(){
+  if(keyCode===32){
+      slingshot.attach(bird.body)
+
+  }
+}
